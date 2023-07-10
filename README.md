051c87275e6bc6a9518064a469e71b792e2d0a20
Alexey Dzyuba <a.dzyuba@gmail.com>
2023-07-10 11:07:09 +0300

    add slopes based method of energy calc

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # preamp-calib
 A software to perform calibration of pre-amplifiers for the time-projection chamber (TPC) readout.
+
+## Interactive scripts
+
+`read_file.py` - draw pair PicoScope's spectra (channels A ans B) from one measurement
+
+`process_dir.py` - analyse sample of measurements
+
+## Energy calculation
+
+Two mwthods are used. In both simple energy summation is used, i.e.
+histogram approximation: `energy += (x[i]+x[i+1])*(0.5*(y[i]+y[i+1]))`
+
+### Baseline based method
+
+Go left (right) from the peak position and sum energies 
+untill it is higher than left (right) baseline.
+
+### Slopes based method
+
+Find left (right) point as an intersept of left (right) slopes with left (right) baseline.
+Slopes treated as lenear functions between 15% and 85% of maximum.
+
+### Comparison of method
+
+Slopes based mathod is found to be more effective extimate as it results with
+energy distribution with smaller standard deviation wrt. baseline based method.
+
+ * StDev Energy (slopes    based) = 0.0187
+ * StDev Energy (baselines based) = 0.0275
+
--- process_dir.py
+++ process_dir.py
@@ -1,7 +1,9 @@
 #===============================================================================
-DIR_PATH = "20230530-0001/"
+DIR_PATH     = "../20230530-0001/"
 LEFT_BORDER  = 850
 RIGHT_BORDER = 1300
+Y_LOW        = 0.15
+Y_HIGH       = 0.85
 #===============================================================================
 import os
 import numpy as np
@@ -55,18 +57,81 @@ def calc_energy_baseline(spec,chan,max,left_base,right_base):
     i_max = i
     return (energy, i_min, i_max )
 
+def find_x(y,x1,y1,x2,y2):
+    if x1==x2:
+        print("ERROR: find_x() : x1==x2 ==> same x")
+        return 0.
+    if y1==y2:
+        print("ERROR: find_x() : y1==y2 ==> any x possible")
+        return 0.
+    return x1 + (y-y1)*(x2-x1)/(y2-y1)
+
+def calc_energy_slopes(spec,chan,max,left_base,right_base,y_low=Y_LOW,y_high=Y_HIGH):
+    y_low = spec[2][1][chan]*y_low
+    y_high= spec[2][1][chan]*y_high
+    energy = 0.
+
+    # go left
+    i = chan
+    while spec[2][1][i]>y_high:
+        energy += ( (spec[1][i]-spec[1][i-1])*0.5*( spec[2][1][i]+spec[2][1][i+1] ) )
+        if spec[2][1][i-1]<y_high:
+            i_high = i-1
+        i-=1
+    while spec[2][1][i]>y_low:
+        energy += ( (spec[1][i]-spec[1][i-1])*0.5*( spec[2][1][i]+spec[2][1][i+1] ) )
+        if spec[2][1][i-1]<y_low:
+            i_low = i-1
+        i-=1
+    i_min = int( find_x(left_base,i_low,spec[2][1][i_low],i_high,spec[2][1][i_high]) )
+#    i_min = int( find_x(left_base,i_low,y_low,i_high,y_high) )
+    while i>i_min:
+        energy += ( (spec[1][i]-spec[1][i-1])*0.5*( spec[2][1][i]+spec[2][1][i+1] ) )
+        i-=1
+
+    # go right
+    i = chan
+    while spec[2][1][i]>y_high:
+        energy += ( (spec[1][i+1]-spec[1][i])*0.5*( spec[2][1][i]+spec[2][1][i+1] ) )
+        if spec[2][1][i+1]<y_high:
+            i_high = i+1
+        i+=1
+    while spec[2][1][i]>y_low:
+        energy += ( (spec[1][i+1]-spec[1][i])*0.5*( spec[2][1][i]+spec[2][1][i+1] ) )
+        if spec[2][1][i+1]<y_low:
+            i_low = i+1
+        i+=1
+    i_max = int( find_x(left_base,i_low,spec[2][1][i_low],i_high,spec[2][1][i_high]) )
+#    i_max = int( find_x(left_base,i_low,y_low,i_high,y_high) )
+    while i<i_max:
+        energy += ( (spec[1][i+1]-spec[1][i])*0.5*( spec[2][1][i]+spec[2][1][i+1] ) )
+        i+=1
+
+    return (energy, i_min, i_max )
+
+def calc_energy(spec,chan,max,left_base,right_base,method="slopes"):
+    if method=="slopes":
+        return calc_energy_slopes(spec,chan,max,left_base,right_base)
+    if method=="baseline":
+        return calc_energy_baseline(spec,chan,max,left_base,right_base)
+    return calc_energy_slopes(spec,chan,max,left_base,right_base)
+
 def analyze_spectrum(spec,left_border=LEFT_BORDER, right_border=RIGHT_BORDER):
      chan       = np.argmax ( spec[2][1]        )
      val        = np.max    ( spec[2][1]        )
      left_base  = np.average( spec[2][1][0:left_border] )
      right_base = np.average( spec[2][1][right_border:] )
-     energy     = calc_energy_baseline(spec,chan,max,left_base,right_base)
+#     energy     = calc_energy(spec,chan,max,left_base,right_base,method="baseline")
+     energy     = calc_energy(spec,chan,max,left_base,right_base,method="slopes")
+#     energy     = calc_energy_baseline(spec,chan,max,left_base,right_base)
 #     return ( np.argmax(spec[2][1]) , np.max(spec[2][1]), np.average( spec[2][1][0:850] ) , np.average( spec[2][1][1300:] ) )
      return ( chan , val, left_base, right_base , energy )
 
 r = get_data(DIR_PATH)
 
 energies = []
+starts   = []
+stops    = []
 
 for s in r:
     m = analyze_spectrum(s)
@@ -81,9 +146,12 @@ for s in r:
     ss += "\t" + str( m[4][1] )
     ss += "\t" + str( m[4][2] )
     energies.append(m[4][0])
+    starts.append(m[4][1])
+    stops.append(m[4][2])
     print(ss)
 
 print("Mean  Energy = " +str(stat.mean(energies))+" +/- "+str(stat.stdev(energies)/np.sqrt(1000.)))
 print("StDev Energy = " +str(stat.stdev(energies)))
+
 plt.hist( energies )
 plt.show()
