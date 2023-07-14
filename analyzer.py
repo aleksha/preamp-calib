9a5a0fb341c16a3bd3e66d9bd64769aee88ee15a
Alexey Dzyuba <a.dzyuba@gmail.com>
2023-07-14 14:07:32 +0200

    next version of app

--- analyzer.py
+++ analyzer.py
@@ -14,6 +14,30 @@ import numpy as np
 #import matplotlib.pyplot as plt
 import statistics as stat
 
+import tkinter as tk
+from tkinter.ttk import Combobox
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+from datetime import datetime
+
+window = tk.Tk()
+
+start_path_to_folder = "./20230530-0001/"
+path_to_folder = tk.StringVar(value=start_path_to_folder)
+
+methods = ["slopes", "baseline"]
+method_var = tk.StringVar(value=methods[0]) 
+
+left_border_var  = tk.IntVar(value=LEFT_BORDER ) 
+right_border_var = tk.IntVar(value=RIGHT_BORDER) 
+low_threshold_var  = tk.IntVar(value=int(Y_LOW*100.) ) 
+high_threshold_var = tk.IntVar(value=int(Y_HIGH*100.)) 
+
 def get_files(dir_path,postfix=".txt"):
     res = []
     for path in os.listdir(dir_path):
@@ -71,6 +95,8 @@ def find_x(y,x1,y1,x2,y2):
     return x1 + (y-y1)*(x2-x1)/(y2-y1)
 
 def calc_energy_slopes(spec,chan,max,left_base,right_base,y_low=Y_LOW,y_high=Y_HIGH):
+    y_low = low_threshold_var.get()/100.
+    y_high= high_threshold_var.get()/100.
     y_low = spec[2][1][chan]*y_low
     y_high= spec[2][1][chan]*y_high
     energy = 0.
@@ -113,7 +139,8 @@ def calc_energy_slopes(spec,chan,max,left_base,right_base,y_low=Y_LOW,y_high=Y_H
 
     return (energy, i_min, i_max, x_min, x_max )
 
-def calc_energy(spec,chan,max,left_base,right_base,method="slopes"):
+def calc_energy(spec,chan,max,left_base,right_base):
+    method = method_var.get()
     if method=="slopes":
         return calc_energy_slopes(spec,chan,max,left_base,right_base)
     if method=="baseline":
@@ -121,15 +148,19 @@ def calc_energy(spec,chan,max,left_base,right_base,method="slopes"):
     return calc_energy_slopes(spec,chan,max,left_base,right_base)
 
 def analyze_spectrum(spec,left_border=LEFT_BORDER, right_border=RIGHT_BORDER):
-     chan       = np.argmax ( spec[2][1]        )
-     val        = np.max    ( spec[2][1]        )
-     left_base  = np.average( spec[2][1][0:left_border] )
-     right_base = np.average( spec[2][1][right_border:] )
+    left_border = left_border_var.get()
+    right_border = right_border_var.get()
+    chan       = np.argmax ( spec[2][1]        )
+    val        = np.max    ( spec[2][1]        )
+    left_in    = np.average( spec[2][0][0:left_border] )
+    right_in   = np.average( spec[2][0][right_border:] )
+    left_base  = np.average( spec[2][1][0:left_border] )
+    right_base = np.average( spec[2][1][right_border:] )
 #     energy     = calc_energy(spec,chan,max,left_base,right_base,method="baseline")
-     energy     = calc_energy(spec,chan,max,left_base,right_base,method="slopes")
+    energy     = calc_energy(spec,chan,max,left_base,right_base)
 #     energy     = calc_energy_baseline(spec,chan,max,left_base,right_base)
 #     return ( np.argmax(spec[2][1]) , np.max(spec[2][1]), np.average( spec[2][1][0:850] ) , np.average( spec[2][1][1300:] ) )
-     return ( chan , val, left_base, right_base , energy )
+    return ( chan , val, left_base, right_base , energy, left_in, right_in, left_in - right_in )
 
 
 def process_dir( path_to_dir , fig):
@@ -139,6 +170,7 @@ def process_dir( path_to_dir , fig):
     starts    = []
     stops     = []
     durations = []
+    gen       = []
 
     for s in r:
         m = analyze_spectrum(s)
@@ -146,10 +178,27 @@ def process_dir( path_to_dir , fig):
         starts.append(m[4][3])
         stops.append(m[4][4])
         durations.append( m[4][4]-m[4][3])
+        gen.append( m[7])
 
-    ss  = path_to_dir
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    ss  = "================================================================================="
+    ss += "\n  Date and time : " + dt_string
+    ss += "\n------------------------------ CONDITIONS ---------------------------------------"
+    ss += "\n  Folder  : " + path_to_dir
+    ss += "\n  Method  : " + str(method_var.get())
+    if str(method_var.get())=="slopes":
+        ss += "    (linear part in [" + str(low_threshold_var.get()) + ","
+        ss += str(high_threshold_var.get())+ "]% of spectrum height)"
+    ss += "\n  Borders : [" + str(left_border_var.get()) + "," + str(right_border_var.get())+ "]"
+    ss += "\n------------------------------ RESULTS ------------------------------------------"
+    ss += "\n  Mean  Input           : " + str(stat.mean(gen))
+    ss += "\n  StDev of Mean Input   : " + str(stat.stdev(gen)/np.sqrt(float(len(gen))))
+    ss += "\n  Median Input          : " + str(stat.median(gen))
+    ss += "\n  StDev of Input        : " + str(stat.stdev(gen))
     ss += "\n  Mean  Energy          : " + str(stat.mean(energies))
-    ss += "\n  StDev of Mean Energy  : " + str(stat.stdev(energies)/np.sqrt(1000.))
+    ss += "\n  StDev of Mean Energy  : " + str(stat.stdev(energies)/np.sqrt(float(len(energies))))
     ss += "\n  Median Energy         : " + str(stat.median(energies))
     ss += "\n  StDev of Energy       : " + str(stat.stdev(energies))
     ss += "\n  Mean of Start Time    : " + str(stat.mean(starts))
@@ -157,7 +206,8 @@ def process_dir( path_to_dir , fig):
     ss += "\n  Mean of Stop Time     : " + str(stat.mean(stops))
     ss += "\n  StDev of Stop Time    : " + str(stat.stdev(stops))
     ss += "\n  Mean of Duration      : " + str(stat.mean(durations))
-    ss += "\n  StDev of Duration     : " + str(stat.stdev(durations)) + "\n\n"
+    ss += "\n  StDev of Duration     : " + str(stat.stdev(durations))
+    ss += "\n=================================================================================\n\n"
 
     for idx in range(len(r)):
         if idx==0:
@@ -177,22 +227,14 @@ def process_dir( path_to_dir , fig):
 #    plt.clear()
     plt.plot(r[0][1],av,"b-")
     plt.plot(r[SHOW_SPEC][1],r[SHOW_SPEC][2][1],"r-")
-
-    rr = {"energies":energies,"starts":starts,"stops":stops,"durations":durations,"data":r,"av":av}
+    plt.set_xlabel("Time [us]")
+    plt.set_ylabel("Signal [a.u.]")
+    rr = {"energies":energies,"starts":starts,"stops":stops,"durations":durations,"data":r,"av":av,"gen":gen}
     return (ss, rr)
 
 #===============================================================================
 # GUI
 #===============================================================================
-
-
-import tkinter as tk
-from tkinter.filedialog import askopenfilename, asksaveasfilename
-
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 
 
 
@@ -223,15 +265,11 @@ def save_file():
     window.title(f"Simple Text Editor - {filepath}")
 
 
-window = tk.Tk()
 window.title("PicoScope's Spectra Analysis")
 
-#window.rowconfigure(0, minsize=50, weight=1)
-#window.columnconfigure(1, minsize=100, weight=1)
 
-start_path_to_folder = "./20230530-0001/"
-path_to_folder = tk.StringVar(value=start_path_to_folder)
 txt_edit = tk.Text(window, width=100)
+
 
 def browse_button():
     # Allow user to select a directory and store it in global var
@@ -258,6 +296,10 @@ def upd_result(txt=txt_edit,fig=fig_draw):
     last_res = res
     return res
 
+xlab = {  "energies":"Energy [a.u.]",
+          "starts":"Signal start [ch.]","stops":"Signal stop [ch.]",
+          "durations":"Signal duration [ch.]","gen":"Input signal [a.u]"}
+
 def draw_canv(what="energies",txt=txt_edit,fig=fig_draw):
     global last_res
     if last_res==None:
@@ -266,6 +308,8 @@ def draw_canv(what="energies",txt=txt_edit,fig=fig_draw):
     plt = fig.add_subplot(111)
     plt.clear()
     plt.hist( last_res[1][what] )
+    plt.set_xlabel(xlab[what])
+    plt.set_ylabel("Entries")
     canvas.draw()
 
 def draw_energies(txt=txt_edit,fig=fig_draw):
@@ -280,8 +324,10 @@ def draw_stops(txt=txt_edit,fig=fig_draw):
 def draw_durations(txt=txt_edit,fig=fig_draw):
     draw_canv("durations",txt,fig)
 
+def draw_input(txt=txt_edit,fig=fig_draw):
+    draw_canv("gen",txt,fig)
+
 def draw_spectra(txt=txt_edit,fig=fig_draw):
-    draw_canv("durations",txt,fig)
     global last_res
     if last_res==None:
         last_res = upd_result(txt,fig)
@@ -290,6 +336,20 @@ def draw_spectra(txt=txt_edit,fig=fig_draw):
     plt.clear()
     plt.plot(last_res[1]["data"][0][1],last_res[1]["av"],"b-")
     plt.plot(last_res[1]["data"][SHOW_SPEC][1],last_res[1]["data"][SHOW_SPEC][2][1],"r-")
+    plt.set_xlabel("Time [us]")
+    plt.set_ylabel("Signal [a.u.]")
+    canvas.draw()
+
+def draw_scatter(txt=txt_edit,fig=fig_draw):
+    global last_res
+    if last_res==None:
+        last_res = upd_result(txt,fig)
+    fig.clear()
+    plt = fig.add_subplot(111)
+    plt.clear()
+    plt.scatter(last_res[1]["gen"],last_res[1]["energies"])
+    plt.set_xlabel("Input signal [a.u]")
+    plt.set_ylabel('Energy [a.u.]')
     canvas.draw()
 
 def dummy():
@@ -318,6 +378,35 @@ btn_save.grid(row=0,column=4)
 
 frame_input.grid(row=0,column=0)
 
+
+
+frame_control = tk.Frame(window)
+lbl_method = tk.Label(frame_control, text="Method: ")
+cbx_method = Combobox(frame_control, textvariable=method_var, values=methods)
+
+lbl_left = tk.Label(frame_control, text="Left border [ch]: ")
+ent_left = tk.Entry(frame_control, textvariable=left_border_var, width=5)
+lbl_right= tk.Label(frame_control, text="Right border [ch]: ")
+ent_right= tk.Entry(frame_control, textvariable=right_border_var, width=5)
+
+lbl_low  = tk.Label(frame_control, text="Low threshold [%]: ")
+ent_low  = tk.Entry(frame_control, textvariable=low_threshold_var, width=4)
+lbl_high = tk.Label(frame_control, text="High threshold [%]: ")
+ent_high = tk.Entry(frame_control, textvariable=high_threshold_var, width=4)
+
+lbl_method.grid(row=0,column=0)
+cbx_method.grid(row=0,column=1)
+lbl_left.grid(row=0,column=2)
+ent_left.grid(row=0,column=3)
+lbl_right.grid(row=0,column=4)
+ent_right.grid(row=0,column=5)
+lbl_low.grid(row=0,column=6)
+ent_low.grid(row=0,column=7)
+lbl_high.grid(row=0,column=8)
+ent_high.grid(row=0,column=9)
+
+frame_control.grid(row=1,column=0)
+
 frame_draw = tk.Frame(window)
 lbl_draw = tk.Label(frame_draw, text="Draw buttons: ")
 btn_energies  = tk.Button(frame_draw, text="Energies", command=draw_energies )
@@ -325,22 +414,26 @@ btn_starts    = tk.Button(frame_draw, text="Starts", command=draw_starts )
 btn_stops     = tk.Button(frame_draw, text="Stops", command=draw_stops  )
 btn_durations = tk.Button(frame_draw, text="Durations", command=draw_durations  )
 btn_spectra   = tk.Button(frame_draw, text="Spectra", command=draw_spectra  )
+btn_input     = tk.Button(frame_draw, text="Input", command=draw_input )
+btn_scatter   = tk.Button(frame_draw, text="Scatter", command=draw_scatter )
 lbl_draw.grid(row=0,column=0)
 btn_energies.grid(row=0,column=1)
 btn_starts.grid(row=0,column=2)
 btn_stops.grid(row=0,column=3)
 btn_durations.grid(row=0,column=4)
 btn_spectra.grid(row=0,column=5)
-frame_draw.grid(row=1,column=0)
+btn_input.grid(row=0,column=6)
+btn_scatter.grid(row=0,column=7)
+frame_draw.grid(row=2,column=0)
 
 
 #canvas.get_tk_widget().grid(row=2,column=0,side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-canvas.get_tk_widget().grid(row=2,column=0)
+canvas.get_tk_widget().grid(row=3,column=0)
 
 
-txt_edit.grid(row=3,column=0)
+txt_edit.grid(row=4,column=0)
 v=tk.Scrollbar(window, orient='vertical', command=txt_edit.yview)
-v.grid(row=2,column=1,sticky='nsew')
+v.grid(row=4,column=1,sticky='nsew')
 
 txt_edit['yscrollcommand'] = v.set
 #v.config(command=txt_edit.yview)
